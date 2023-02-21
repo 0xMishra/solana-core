565abb68afb59c69ffcd93c4a031b688b6cd58fc
mishra811 <mishrakundan073@gmail.com>
2023-02-21 17:56:55 +0530

    separation of concerns in movie program

--- core-3/movie_review/src/state.rs
+++ core-3/movie_review/src/state.rs
@@ -1,4 +1,5 @@
 use borsh::{BorshDeserialize, BorshSerialize};
+use solana_program::program_pack::{IsInitialized, Sealed};
 
 #[derive(BorshSerialize, BorshDeserialize)]
 pub struct MovieAccountState {
@@ -8,3 +9,10 @@ pub struct MovieAccountState {
     pub description: String,
 }
 
+impl Sealed for MovieAccountState {}
+
+impl IsInitialized for MovieAccountState {
+    fn is_initialized(&self) -> bool {
+        self.is_initialized
+    }
+}
